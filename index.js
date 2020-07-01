c7be62207bc640ce6e3824f09686dcca0744834c
BingShawLim <shaw.a.muvee@gmail.com>
2020-06-30 22:42:50 -0500

    setup express-handlebars

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-const express = require('express')
-const app = express()
-const path = require('path')
+const express = require('express');
+const app = express();
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.get('/', (req, res)=> {
+    res.redirect('home')
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
